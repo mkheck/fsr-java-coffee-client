dd82bf2a0a74b4dc5b4f34bacb92714030204674
Mark Heckler <mark.heckler@gmail.com>
2018-11-03 14:07:47 +0800

    Initial commit

---
+++ src/main/java/com/thehecklers/fsrjavacoffeeclient/FsrJavaCoffeeClientApplication.java
@@ -0,0 +1,12 @@
+package com.thehecklers.fsrjavacoffeeclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsrJavaCoffeeClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FsrJavaCoffeeClientApplication.class, args);
+	}
+}
---
+++ src/test/java/com/thehecklers/fsrjavacoffeeclient/FsrJavaCoffeeClientApplicationTests.java
@@ -0,0 +1,16 @@
+package com.thehecklers.fsrjavacoffeeclient;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FsrJavaCoffeeClientApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
